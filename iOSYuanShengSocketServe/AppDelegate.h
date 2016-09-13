2a1e06170ef13baedb0849275f1906643b249a65
ataw <ataw@atawdemac-mini.local>
2016-09-13 10:16:45 +0800

    Initial Commit

---
+++ iOSYuanShengSocketServe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSYuanShengSocketServe
+//
+//  Created by ataw on 16/9/13.
+//  Copyright © 2016年 王宗成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSYuanShengSocketServe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSYuanShengSocketServe
+//
+//  Created by ataw on 16/9/13.
+//  Copyright © 2016年 王宗成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
